9405859b18f9c0a1e6c736c597b1845e70ced398
Priit Haamer <priit@fraktal.ee>
2014-08-09 13:17:08 +0300

    Initial Commit

---
+++ EstimoteBeacons/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EstimoteBeacons
+//
+//  Created by Priit Haamer on 09.08.14.
+//  Copyright (c) 2014 Priit Haamer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
