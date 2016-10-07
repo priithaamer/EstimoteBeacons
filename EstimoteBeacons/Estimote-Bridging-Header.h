4b8e5e42322e82341660a0aa5a63a156312979bc
Priit Haamer <priit@haamer.ee>
2016-10-07 14:03:50 +0300

    Second commit

---
+++ EstimoteBeacons/Estimote-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Estimote-Bridging-Header.h
+//  EstimoteBeacons
+//
+//  Created by Priit Haamer on 09.08.14.
+//  Copyright (c) 2014 Priit Haamer. All rights reserved.
+//
+
+//#ifndef EstimoteBeacons_Estimote_Bridging_Header_h
+//#define EstimoteBeacons_Estimote_Bridging_Header_h
+//
+//
+//#endif
+
+#import "ESTBeaconManager.h"
--- EstimoteBeacons/ViewController.swift
+++ EstimoteBeacons/ViewController.swift
@@ -8,11 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-                            
+let uuid = NSUUID(UUIDString: "B9407F30-F5F8-466E-AFF9-25556B57FE6D")
+
+class ViewController: UIViewController, ESTBeaconManagerDelegate {
+    
+    let beaconManager = ESTBeaconManager()
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.requestAlwaysAuthorization()
+        
+        var region = ESTBeaconRegion(proximityUUID: uuid, identifier: "region")
+        
+        beaconManager.delegate = self
+        beaconManager.avoidUnknownStateBeacons = true
+        
+        beaconManager.startMonitoringForRegion(region)
+        beaconManager.requestStateForRegion(region)
+        
+        // beaconManager.startRangingBeaconsInRegion(region)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +38,39 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func beaconManager(manager: ESTBeaconManager!, didEnterRegion region: ESTBeaconRegion!) {
+        NSLog("Did enter region")
+        
+        // present local notification
+        var notification = UILocalNotification()
+        notification.alertBody = "Tere tulemast!"
+        notification.soundName = UILocalNotificationDefaultSoundName
+        
+        UIApplication.sharedApplication().presentLocalNotificationNow(notification)
+    }
+    
+    func beaconManager(manager: ESTBeaconManager!, didExitRegion region: ESTBeaconRegion!) {
+        NSLog("Did exit region")
+        
+        var notification = UILocalNotification()
+        notification.alertBody = "Nägemist!"
+        notification.soundName = UILocalNotificationDefaultSoundName
+        
+        UIApplication.sharedApplication().presentLocalNotificationNow(notification)
+    }
+    
+    func beaconManager(manager: ESTBeaconManager!, didDetermineState state: CLRegionState, forRegion region: ESTBeaconRegion!) {
+        NSLog("Did determine state")
+        
+        if state == .Inside {
+            NSLog("State is inside")
+        } else {
+            NSLog("State is outside")
+        }
+    }
 
+    // func beaconManager(manager: ESTBeaconManager!, didRangeBeacons beacons: [AnyObject]!, inRegion region: ESTBeaconRegion!) {
+    //     NSLog("Found \(beacons.count) beacons in range")
+    // }
 }
 
